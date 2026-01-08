c43bdd50a1d7fed5aeb664c66d0c1c4eafba2aea
CyberSphinxx <johnlemargonzales@gmail.com>
2026-01-09 05:09:21 +0800

    Fix potential lingering tooltip issues

--- src/gui/tooltip.py
+++ src/gui/tooltip.py
@@ -2,23 +2,33 @@ import customtkinter as ctk
 
 class ToolTip:
     """
-    Lightweight tooltip that appears on hover.
+    Lightweight tooltip that appears on hover with a delay.
     """
-    def __init__(self, widget, text):
+    def __init__(self, widget, text, delay=500):
         self.widget = widget
         self.text = text
+        self.delay = delay
         self.tip_window = None
+        self.id = None
         
-        self.widget.bind("<Enter>", self._show_tip)
+        self.widget.bind("<Enter>", self._schedule_tip)
         self.widget.bind("<Leave>", self._hide_tip)
+        self.widget.bind("<ButtonPress>", self._hide_tip)
     
+    def _schedule_tip(self, event=None):
+        self._hide_tip()
+        self.id = self.widget.after(self.delay, self._show_tip)
+        
     def _show_tip(self, event=None):
-        if self.tip_window or not self.text:
+        if not self.text:
             return
         
         # Get position below the widget
-        x = self.widget.winfo_rootx() + 10
-        y = self.widget.winfo_rooty() + self.widget.winfo_height() + 5
+        try:
+            x = self.widget.winfo_rootx() + 10
+            y = self.widget.winfo_rooty() + self.widget.winfo_height() + 5
+        except Exception:
+            return # Widget might be destroyed
         
         self.tip_window = tw = ctk.CTkToplevel(self.widget)
         tw.wm_overrideredirect(True)  # Remove window decorations
@@ -38,6 +48,10 @@ class ToolTip:
         label.pack()
     
     def _hide_tip(self, event=None):
+        if self.id:
+            self.widget.after_cancel(self.id)
+            self.id = None
+            
         if self.tip_window:
             self.tip_window.destroy()
             self.tip_window = None
